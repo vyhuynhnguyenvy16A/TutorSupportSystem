80a3448753fb26e3fd3721c2a303a45fdec3d8d6
vyhuynhnguyenvy16A <vy.huynhnguyenvy@hcmut.edu.vn>
2026-03-31 11:18:24 +0700

    fix

--- src/components/Layout/AuthLayout.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function AuthLayout({ children }) {
-  return (
-    <div className="auth-layout" style={{ padding: 20 }}>
-      <div>{children}</div>
-    </div>
-  );
-}
--- src/pages/admin/Dashboard.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function AdminDashboard() {
-  return (
-    <div>
-      <h2>Admin Dashboard</h2>
-    </div>
-  );
-}
